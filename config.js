24fbd39a37df5b8a528245ffa01c72049359d19a
supnumb <supnumb@163.com>
2018-04-23 15:08:56 +0800

    代码目录结构的调整

---
+++ config.js
@@ -0,0 +1,21 @@
+var config = {
+
+    DB_TEST: {
+        host: 'www.loclive.cn',
+        connectionLimit: 50,
+        user: "chenxy",
+        password: "60v&iA",
+        database: "Medicine"
+    },
+
+    DB_PRO: {
+        host: 'www.loclive.cn',
+        connectionLimit: 50,
+        user: "chenxy",
+        port: "3360",
+        password: "60v&iA",
+        database: "Medicine"
+    }
+};
+
+module.exports = config;
