1a34c1830f45b3182a0edd4e2658285e5895adc1
Chen-xy <abchenxy@gmail.com>
2018-04-27 13:23:07 +0800

    add orderList

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('orderList');
+});
+
+router.get('/memberList', function(req, res, next) {
     res.render('index');
 });
 
